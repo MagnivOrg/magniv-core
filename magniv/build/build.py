4534af8bfe865321a2eec6e9ff6af009f31733d0
sean abreau <92339393+seanabreau@users.noreply.github.com>
2022-06-12 08:51:35 -0700

    Update build.py

--- magniv/build/build.py
+++ magniv/build/build.py
@@ -31,32 +31,40 @@ def build():
     for root, dirs, files in os.walk("./tasks"):
         if os.path.exists("./tasks/requirements.txt"):
             root_req = "./tasks/requirements.txt"
+        req = (
+            "{}/requirements.txt".format(root)
+            if os.path.exists("{}/requirements.txt".format(root))
+            else root_req
+        )
+        if req == None:
+            raise OSError(
+                f'requirements.txt not found for path "{root}", either add one to this directory or the root directory'
+            )
         for file_name in files:
             ext = os.path.splitext(file_name)[-1].lower()
-            if ext == ".py":
-                filepath = "{}/{}".format(root, file_name)
-                with open(filepath) as file:
-                    parsed_ast = ast.parse(file.read())
-                tasks = []
+        if ext == ".py":
+            filepath = "{}/{}".format(root, file_name)
+            with open(filepath) as file:
+                parsed_ast = ast.parse(file.read())
                 for node in ast.walk(parsed_ast):
                     if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
                         if len(node.decorator_list) > 0:
                             for decorator in node.decorator_list:
-                                if not isinstance(decorator, ast.Name) and decorator.func.id == 'task':
-                                    pdb.set_trace()
-                                    tasks.append(node)
-                for task in tasks:
-                    tasks_list.append(task.name)
-                print(tasks_list)
-                req = (
-                    "{}/requirements.txt".format(root)
-                    if os.path.exists("{}/requirements.txt".format(root))
-                    else root_req
-                )
-                if req == None:
-                    raise OSError(
-                        f'requirements.txt not found for path "{root}", either add one to this directory or the root directory'
-                    )
+                                if (
+                                    not isinstance(decorator, ast.Name)
+                                    and decorator.func.id == "task"
+                                ):
+                                    for decorator in node.decorator_list:
+                                        info = {
+                                                    "location": filepath,
+                                                    "name": node,
+                                                    "python_version": _get_python_version(root),
+                                                    "owner": _get_owner(root),
+                                                    "requirements_location": req,
+                                                    "line_number": lineno,
+                                                }
+                                        for kw in decorator.keywords:
+                                            info[kw.arg] = kw.value.value                
                 for name, t in tasks:
                     if t.key in used_keys:
                         raise ValueError(
@@ -64,17 +72,6 @@ def build():
                         )
                     else:
                         used_keys[t.key] = filepath
-                    code, lineno = getsourcelines(t)
-                    info = {
-                        "location": filepath,
-                        "name": t.name,
-                        "key": t.key,
-                        "description": t.description,
-                        "schedule": t.schedule,
-                        "python_version": _get_python_version(root),
-                        "owner": _get_owner(root),
-                        "requirements_location": req,
-                        "line_number": lineno,
-                    }
+#                     code, lineno = getsourcelines(t)
                     tasks_list.append(info)
         _save_to_json(tasks_list, filepath="./dump.json")
