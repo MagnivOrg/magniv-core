85a93bf9afc2fff6c554c602b5c14eafa62e19bc
gusrasch <gusrasch@gmail.com>
2022-10-17 10:26:14 -0400

    adds docstring

--- tests/unit_tests/test_invalid_trigger.py
+++ tests/unit_tests/test_invalid_trigger.py
@@ -24,6 +24,18 @@ if __name__ == "__main__":
 
 @pytest.fixture
 def file(tmpdir):
+    """
+    Creates a directory called tasks, creates a file called main.py in that directory, and writes
+    the contents of the TEST_FILE variable to that file. It then creates a file called
+    requirements.txt in the tasks directory and writes the word magniv to it. Finally, it returns
+    the path to the tasks directory
+
+    Args:
+        tmpdir: This is a pytest fixture that creates a temporary directory for us to use.
+
+    Returns:
+        The path to the directory where the file is located.
+    """
     tmpdir.mkdir("tasks").join("main.py").write(TEST_FILE)
     tmpdir.join("tasks/requirements.txt").write("magniv")
     return str(tmpdir.join("tasks"))
