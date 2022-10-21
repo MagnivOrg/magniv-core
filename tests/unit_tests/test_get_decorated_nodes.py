3fe473c4635a13304803e6ae6bd110130be5e2ef
gusrasch <gusrasch@gmail.com>
2022-10-21 17:50:44 -0400

    linting

--- tests/unit_tests/test_get_decorated_nodes.py
+++ tests/unit_tests/test_get_decorated_nodes.py
@@ -1,15 +1,14 @@
 import ast
 from typing import Union
 
-from tests.unit_tests.fixtures.test_files import GET_DECORATED_NODES_FILE
-from tests.unit_tests.test_invalid_build import TestBuildInvalid
-
 import pytest
 
 from magniv.build.build import get_decorated_nodes
+from tests.unit_tests.fixtures.test_files import GET_DECORATED_NODES_FILE
+from tests.unit_tests.test_invalid_build import TestBuildInvalid
 
-class TestDecoratedNodes():
 
+class TestDecoratedNodes:
     @pytest.fixture
     def ast(self) -> Union[ast.AST, str]:
         return ast.parse(GET_DECORATED_NODES_FILE)
@@ -19,7 +18,6 @@ class TestDecoratedNodes():
         assert "magniv.core.task" in aliases
         assert "task" in aliases
         assert "mc.task" in aliases
-
 
     def test_get_decorated_nodes_ignores_non_magniv_decorators(self, ast):
         nodes, aliases = get_decorated_nodes(ast)
