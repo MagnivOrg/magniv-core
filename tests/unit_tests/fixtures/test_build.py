03c81442881d71c97affe4bffd3fa85c12f1b857
gusrasch <gusrasch@gmail.com>
2022-10-22 11:33:43 -0400

    uses tmp_path instead, adds comments

--- tests/unit_tests/fixtures/test_build.py
+++ tests/unit_tests/fixtures/test_build.py
@@ -2,7 +2,20 @@ import pytest
 
 class TestBuild:
     @pytest.fixture
-    def folder(self, tmpdir, file):
-        tmpdir.mkdir("tasks").join("main.py").write(file)
-        tmpdir.join("tasks/requirements.txt").write("magniv")
-        return str(tmpdir.join("tasks"))
+    def folder(self, tmp_path, file):
+        """Uses the pytest builtin fixture tmp_path to create a temporary tasks folder and requirements.txt for each test invocation"""
+        # setup tasks folder
+        tasks_folder = tmp_path / "tasks"
+        tasks_folder.mkdir()
+        # write requirements.txt with magniv
+        reqs = tasks_folder / "requirements.txt"
+        reqs.write_text("magniv")
+        # write contents of test file and return
+        test_file = tasks_folder / "main.py"
+        test_file.write_text(file)
+        return str(tasks_folder)
+
+    @pytest.fixture
+    def file(self):
+       """Method stub for test file"""
+       pass
